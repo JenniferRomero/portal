46238e90f43720e1d3a0c002d1a9e6eedb9e130a
Jairo Burgos Guarín <jyburgos40@gmail.com>
2020-06-24 12:31:41 -0500

    Translate

--- src/app/validate-token/validate-token.module.ts
+++ src/app/validate-token/validate-token.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { ValidateTokenComponent } from './validate-token.component';
 import { TransactionRoutingModule } from './validate-token-routing.module';
 
@@ -7,9 +8,10 @@ import {
   ValidateComponent,
   ErrorComponent
 } from './pages/pages.index';
+
+import { IdCVtStateComponent } from './components/id-c-vt-state/id-c-vt-state.component';
 import { IdCVtHeaderComponent } from './components/id-c-vt-header/id-c-vt-header.component';
 import { IdCVtFooterComponent } from './components/id-c-vt-footer/id-c-vt-footer.component';
-import { IdCVtStateComponent } from './components/id-c-vt-state/id-c-vt-state.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { IdCVtStateComponent } from './components/id-c-vt-state/id-c-vt-state.co
   ],
   imports: [
     CommonModule,
+    TranslateModule,
     TransactionRoutingModule
   ]
 })
